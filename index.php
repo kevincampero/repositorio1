a1197877168b94d1f6bf8ab50890df03f3872e67
kevincampero <kevincampero42@gmail.com>
2021-02-02 18:14:48 -0400

    c

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/index.css">
+    <title>Document</title>
+</head>
+<body>
+    
+    <section>
+        <nav>
+            <img src="img/logo.png" width="8%" alt="">
+            <ul class="lista">
+                <li><a href="#">INICIO</a></li>
+                <li><a href="#">CURSOS</a></li>
+                <li><a href="#">QUIENES SOMOS</a></li>
+                <li><a href="#">CONTACTO</a></li>                
+            </ul>
+        </nav>
+        <div class="content">
+            <h2>
+                <span class="title">TELC</span> The Executive Learning Center <br> <br>
+                <span class="typed"></span> 
+            </h2>
+            <br>
+            <center>
+                <h3>
+                    Certificados con valor curricular
+                </h3>
+            </center>
+            <div class="titulo" id="cadenas-texto">
+                <p>Cursos de <i class="materia"> Inglés </i> </p>
+                <p>Cursos de <i class="materia"> Informatica </i> </p>
+                <p>Cursos de <i class="materia"> Quechua </i> </p>
+            </div>
+                
+        </div>
+    </section>
+    <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.11"></script>
+    <script src="js/main.js"></script>
+</body>
+</html>
---
+++ js/main.js
@@ -0,0 +1,16 @@
+const typed= new Typed('.typed',{
+    /*strings: [
+        '<i class="materia">Inglés</i>',
+        '<i class="materia">Informatica</i>',
+        '<i class="materia">Quechua</i>'
+    ],*/
+    stringsElement:'#cadenas-texto',
+    typeSpeed: 75,
+    backSpeed: 75,
+    shuffle: false,
+    startDelay: 300,
+    backDelay: 1500,
+    loop: true,
+    smartBackspace: true
+
+});
